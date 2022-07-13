692dbddec60dee4b65016cf354e2496e66c4d9d1
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2022-07-13 18:02:16 +0530

    sideMenuBarUX improved

--- ACE-online/ViewConroller/HomeViewController.swift
+++ ACE-online/ViewConroller/HomeViewController.swift
@@ -42,16 +42,105 @@ class HomeViewController: UIViewController, SideMenuBarViewControllerDelegate {
         }
 
     private func hideSideMenuBar(){
-        self.sideMenuBar.isHidden = true
-        self.leadingConstraintsForSideBar.constant = -280
+        
+        UIView.animate(withDuration: 0.1) {
+            self.leadingConstraintsForSideBar.constant = 10
+            self.view.layoutIfNeeded()
+        } completion: { (status) in
+            UIView.animate(withDuration: 0.1) {
+                self.leadingConstraintsForSideBar.constant = -280
+                self.view.layoutIfNeeded()
+            } completion: { (status) in
+                self.sideMenuBar.isHidden = true
+                self.isSideMenuShow = false
+            }
+        }
+
+        
+        
+        
         
     }
     @IBAction func sideMenuButtonPressed(_ sender: UIButton) {
-        sideMenuBar.isHidden = false
-        leadingConstraintsForSideBar.constant = 0
+        UIView.animate(withDuration: 0.1) {
+            self.leadingConstraintsForSideBar.constant = 10
+            self.view.layoutIfNeeded()
+        } completion: { (status) in
+            UIView.animate(withDuration: 0.1) {
+                self.leadingConstraintsForSideBar.constant = 0
+                self.view.layoutIfNeeded()
+            } completion: { (status) in
+                self.sideMenuBar.isHidden = false
+                self.isSideMenuShow = true
+            }
+       
+        }
+        
     }
     
     @IBAction func tappedOnBackView(_ sender: Any) {
         self.hideSideMenuBar()
     }
+    
+  private var isSideMenuShow = false
+  
+    private var beginPoint: CGFloat = 0.0
+    
+    private var difference: CGFloat = 0.0
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(isSideMenuShow){
+            if let touch = touches.first{
+                let location = touch.location(in: sideMenuBar)
+                
+                beginPoint = location.x
+            }
+            
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(isSideMenuShow){
+            if let touch = touches.first{
+                let location = touch.location(in: sideMenuBar)
+                
+                let differenceFromBeginPoint = beginPoint - location.x
+                
+                if(differenceFromBeginPoint > 0 && differenceFromBeginPoint < 280){
+                    self.leadingConstraintsForSideBar.constant = -differenceFromBeginPoint
+                    difference = differenceFromBeginPoint
+                }
+            }
+            
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(isSideMenuShow){
+            if let touch = touches.first{
+                if(difference > 140){
+                    UIView.animate(withDuration: 0.1) {
+                        self.leadingConstraintsForSideBar.constant = -290
+                        self.view.layoutIfNeeded()
+                    } completion: { (status) in
+                        self.sideMenuBar.isHidden = true
+                        self.isSideMenuShow = false
+                    }
+
+                }
+                else{
+                    UIView.animate(withDuration: 0.1) {
+                        self.leadingConstraintsForSideBar.constant = 0
+                        self.view.layoutIfNeeded()
+                    } completion: { (status) in
+                        self.sideMenuBar.isHidden = false
+                        self.isSideMenuShow = true
+                    }
+                    
+                }
+            }
+            
+        }
+    }
 }
+
